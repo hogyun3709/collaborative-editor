f5e1637028cef598252908b790f4757567777263
hogyun3709 <hogyun3709@gmail.com>
2020-09-06 20:35:54 +0900

    React applicaiton setup - sturcture and node packages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './index.css';
+import * as serviceWorker from './serviceWorker';
 
-export default App;
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
